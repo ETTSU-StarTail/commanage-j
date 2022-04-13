2320e714e01062ea6ff4ae2901098d0857e88fdd
echigo <echigo@nsd-ltd.co.jp>
2022-04-13 17:29:29 +0900

    refs #1 Spring Boot プロジェクト作成
    Hello World 向けのシンプルな構成。

---
+++ src/main/java/work/ettsu/commanagej/CommanageJApplication.java
@@ -0,0 +1,13 @@
+package work.ettsu.commanagej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommanageJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommanageJApplication.class, args);
+	}
+
+}
---
+++ src/test/java/work/ettsu/commanagej/CommanageJApplicationTests.java
@@ -0,0 +1,13 @@
+package work.ettsu.commanagej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommanageJApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
