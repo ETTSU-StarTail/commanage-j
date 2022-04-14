6966d73d2612fd27c9ccdeab8599090cee0378ca
ETTSU-StarTail <46245666+ETTSU-StarTail@users.noreply.github.com>
2022-04-14 15:45:28 +0900

    Hello Worlf アプリケーション作成した。
    Hello World アプリケーションを作成 #2

--- src/main/java/work/ettsu/commanagej/CommanageJApplication.java
+++ src/main/java/work/ettsu/commanagej/CommanageJApplication.java
@@ -2,12 +2,21 @@ package work.ettsu.commanagej;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CommanageJApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommanageJApplication.class, args);
+	}
+
+	@GetMapping(value = "/hello")
+	public String getMethodName(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello, %s!", name);
 	}
 
 }
